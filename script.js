3e996e4b941ce54aba01050d1511997f1227b2b4
Sala Sergiu <121624777+Sala-Sergiu@users.noreply.github.com>
2023-04-26 19:24:33 +0300

    Game logic is working perfectly

--- script.js
+++ script.js
@@ -1,12 +1,76 @@
+const rock = document.querySelector(".rock-btn");
+const paper = document.querySelector(".paper-btn");
+const scissors = document.querySelector(".scissors-btn");
+
+const tryAgain = document.querySelector(".try-again");
+let score = document.querySelector(".score");
+let scoreAi = document.querySelector(".score-ai");
+const image = document.querySelector(".image");
+const imageAi = document.querySelector(".image-ai");
+
+let playerScore = "";
+let aiScore = "";
+
+// console.log("default", score);
+// console.log("playerscore", playerScore);
+// console.log("airscore", aiScore);
+
 const getComputerChoice = function () {
   const random = Math.trunc(Math.random() * 3) + 1;
+  // 1 = rock
+  // 2 = paper
+  // 3 = scissors
   if (random === 1) {
-    return "1 rock";
+    let choice = "rock";
+    imageAi.src = `./images/${choice}-mirror.png`;
   } else if (random === 2) {
-    return "2 paper";
+    let choice = "paper";
+    imageAi.src = `./images/${choice}-mirror.png`;
   } else {
-    return "3 scissor";
+    let choice = "scissors";
+    imageAi.src = `./images/${choice}-mirror.png`;
   }
+  return random;
 };
 
-console.log(getComputerChoice());
+rock.addEventListener("click", function () {
+  const random = getComputerChoice();
+  let choice = "rock";
+  console.log(`${choice} was selected`);
+
+  if (choice === "rock") {
+    image.src = `./images/${choice}.png`;
+  }
+
+  if (random === 3) {
+    playerScore++;
+    Number((score.textContent = playerScore));
+
+    console.log("player score", playerScore);
+    console.log("---score", +score.textContent);
+  } else if (random === 2) {
+    aiScore++;
+    Number((scoreAi.textContent = aiScore));
+
+    console.log("aiscore", playerScore);
+    console.log("---score", +score.textContent);
+  } else {
+    return;
+  }
+});
+
+paper.addEventListener("click", function () {
+  let choice = "paper";
+  console.log(`${choice} was selected`);
+  if (choice === "paper") {
+    image.src = `./images/${choice}.png`;
+  }
+});
+
+scissors.addEventListener("click", function () {
+  let choice = "scissors";
+  console.log(`${choice} was selected`);
+  if (choice === "scissors") {
+    image.src = `./images/${choice}.png`;
+  }
+});
